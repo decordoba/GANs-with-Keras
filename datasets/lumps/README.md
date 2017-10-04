094dc74622bb2489d0f6eed6f72a62f54bffc21f
decordoba <danieldecordobagil@hotmail.com>
2017-10-04 17:16:25 -0500

    Updated lumps generator

---
+++ datasets/lumps/README.md
@@ -0,0 +1,13 @@
+# Steps to generate lumps data
+
+1. Edit `dataGenerator.m` and set `FIRST_SAMPLE_NUM` and `LAST_SAMPLE_NUM` to select the number
+   and range of samples, as well as the parameters to generate the lumps.
+
+2. Run `dataGenerator.m` executing `matlab dataGenerator` (it will create two folders: `dataset` and `lumps`. `dataset` is where all the
+   image data is stored, and `lumps` contains some of the images generation data in case we want to use it later
+   in our networks (it holds the locations of each lump)). It is possible to see the data instead of saving it
+   by running `matlab dataGenerator(false)`, but then nothing will be saved inside the two folders mentioned. This
+   can be used for debugging purposes, to check the kind of images that will be generated before creating the dataset.
+
+3. Run 'python3 load_data.py', it will get all the information in `dataset` and save it in the
+   `lumps.npy` file, which has the right `numpy` format to be used in Keras.
--- datasets/lumps/dataGenerator.m
+++ datasets/lumps/dataGenerator.m
@@ -1,29 +1,46 @@
+function dataGenerator(save)
+
 FIRST_SAMPLE_NUM = 0;
 LAST_SAMPLE_NUM = 59999;
-num_samples = LAST_SAMPLE_NUM - FIRST_SAMPLE_NUM + 1
+num_samples = LAST_SAMPLE_NUM - FIRST_SAMPLE_NUM + 1;
+disp(['Samples generated: ', num2str(num_samples)]);
 
 IMAGE_SIZE = [64, 64];
 MEAN_NUMBER_LUMPS = 200;
 DC = 10;
 LUMP_FUNCTION = 'GaussLmp';
-PARS = [1, 10];
+PARS = [1, 2];  % [1, 10]
 
-% create folders
-folder_dataset = 'dataset/';
-folder_lumps = 'lumps/';
-mkdir(folder_dataset);
-mkdir(folder_lumps);
-extension = '.txt';
+SAVE = true;  % true: save images, false: show images
+if nargin > 0
+    SAVE = save;
+end
 
-% save data
+if SAVE
+    % create folders
+    folder_dataset = 'dataset/';
+    folder_lumps = 'lumps/';
+    mkdir(folder_dataset);
+    mkdir(folder_lumps);
+    extension = '.txt';
+else
+    close;
+    disp('Warning, images are only shown, not saved!');
+end
+
+% save or show data
 for i = FIRST_SAMPLE_NUM:LAST_SAMPLE_NUM
     [matrix, num_lumps, pos_lumps] = LumpyBgnd(IMAGE_SIZE, MEAN_NUMBER_LUMPS, DC, LUMP_FUNCTION, PARS);
-    filename = [num2str(i, '%06d'), '_', num2str(num_lumps, '%03d'), extension];
-    dlmwrite([folder_dataset, filename], matrix, 'precision', 6)
-    dlmwrite([folder_lumps, filename], pos_lumps, 'precision', 6)
-    %save([folder_dataset, filename], 'matrix', '-ascii');
-    %save([folder_lumps, filename], 'pos_lumps', '-ascii');
-    
+    if SAVE
+        % save data
+        filename = [num2str(i, '%06d'), '_', num2str(num_lumps, '%03d'), extension];
+        dlmwrite([folder_dataset, filename], matrix, 'precision', 6)
+        dlmwrite([folder_lumps, filename], pos_lumps, 'precision', 6)
+    else
+        % show data
+        imshow(matrix, [])
+    end
+
     if mod(i, 100) == 0
         c = clock;
         disp([num2str(c(4), '%02d'), ':', num2str(c(5), '%02d'), '.', num2str(int8(c(6)), '%02d'), '  ', 'It. ', num2str(i)])
