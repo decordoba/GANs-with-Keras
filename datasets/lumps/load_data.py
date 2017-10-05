2e3384a3381a26a349965213b906468deee5871f
decordoba <danieldecordobagil@hotmail.com>
2017-10-05 01:34:15 -0500

    load_data shows time now, to monitor process better

--- datasets/lumps/load_data.py
+++ datasets/lumps/load_data.py
@@ -2,7 +2,17 @@
 
 import os
 import numpy as np
+from datetime import datetime
 
+
+def getCurrentTime(time=True, date=False):
+    now = datetime.now()
+    s = ""
+    if date:
+        s += "{} ".format(now.date())
+    if time:
+        s += "{:02d}:{:02d}:{:02d}".format(now.hour, now.minute, now.second)
+    return s.strip()
 
 def load_data(dataset, filename_out="lumps"):
     if dataset == "lumps":
@@ -11,7 +21,7 @@ def load_data(dataset, filename_out="lumps"):
         raise KeyError("Unknown dataset: {}".format(dataset))
     files = sorted(os.listdir(path))
     num_files = len(files)
-    percent = 10
+    percent = 2
     split_distance = num_files * percent // 100
     print("Loading {} samples from dataset {}".format(num_files, dataset))
     for i, filename in enumerate(files):
@@ -23,7 +33,7 @@ def load_data(dataset, filename_out="lumps"):
             except NameError:
                 data = np.array([sample])
         if (i + 1) % split_distance == 0:
-            print("{}% loaded".format((i + 1) * 100 // num_files))
+            print("{}. {}% loaded".format(getCurrentTime(), (i + 1) * 100 // num_files))
     print("Shape:", data.shape)
     np.save(filename_out, data)
 
